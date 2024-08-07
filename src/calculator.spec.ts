e64ee98542ae5a1dbf64268ad7fbddf110f7eedc
Michel Kim <kim98.michel@gmail.com>
2024-08-06 21:19:49 -0300

    feat: add basics calculate methods sum, minus, times and dvide

--- src/calculator.spec.ts
+++ src/calculator.spec.ts
@@ -1,3 +1,5 @@
+import { Calculator } from "./calculator"
+
 describe('Calculator', () => {
     test('Should sum two numbers', () => {
         const sut = new Calculator()
@@ -7,5 +9,35 @@ describe('Calculator', () => {
 
         const result = sut.sum(a, b)
         expect(result).toBe(8)
+    })
+
+    test('Should minus two numbers', () => {
+        const sut = new Calculator()
+
+        const a = 5
+        const b = 3
+
+        const result = sut.minus(a, b)
+        expect(result).toBe(2)
+    })
+
+    test('Should multiply two numbers', () => {
+        const sut = new Calculator()
+
+        const a = 5
+        const b = 3
+
+        const result = sut.multiply(a, b)
+        expect(result).toBe(15)
+    })
+
+    test('Should multiply divide numbers', () => {
+        const sut = new Calculator()
+
+        const a = 15
+        const b = 3
+
+        const result = sut.divide(a, b)
+        expect(result).toBe(5)
     })
 })
---
+++ src/calculator.ts
@@ -0,0 +1,17 @@
+export class Calculator {
+    sum(a: number, b: number) {
+        return a + b
+    }
+
+    minus(a: number, b: number) {
+        return a - b
+    }
+
+    multiply(a: number, b: number) {
+        return a * b
+    }
+
+    divide(a: number, b: number) {
+        return a / b
+    }
+}
